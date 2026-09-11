8eb1e4a59578424c88a149afff8b39a2876147a0
junhui <junhuilim@kakao.com>
2026-09-12 01:38:16 +0900

    chore: initialize Spring Boot project

---
+++ src/test/java/com/supplierhub/SupplierHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supplierhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplierHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
